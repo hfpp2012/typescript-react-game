4fb66d60f243c1144c7f1d51ae8fb66a3fb17de6
hfpp2012 <hfpp2012@gmail.com>
2019-03-27 16:10:21 +0800

    08_start

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import Func from './components/Func';
 
 class App extends Component {
+
+  state = {
+    board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }
+
+  public renderCell = (index: number) => {
+    return (
+      <div className="cell"></div>
+    )
+  }
+
+  public renderBoard = () => {
+    return (
+      <div className="board-container">
+        { this.state.board.map((value, key) => this.renderCell(key)) }
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <Form />
-        <Func name="rails365" age={ 12 } />
+        { this.renderBoard() }
       </div>
     );
   }
