ee4d6e9506fe0af7756026d00071183b16ec5797
hfpp2012 <hfpp2012@gmail.com>
2019-03-28 16:09:34 +0800

    09_players

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+enum Player {
+  None = 0,
+  One = 1,
+  Two = 2
+}
+
+interface IState {
+  board: Player[];
+  nextPlayerTurn: Player
+}
+
+class App extends Component<{}, IState> {
 
   state = {
-    board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    board: [Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None, 
+            Player.None],
+    nextPlayerTurn: Player.One
+  }
+
+  public handleCellClick = (index: number) => {
+    const { board, nextPlayerTurn } = this.state;
+
+    let newBoard = board.slice();
+
+    newBoard[index] = nextPlayerTurn
+
+    this.setState({
+      board: newBoard,
+      nextPlayerTurn: 3 - nextPlayerTurn
+    })
   }
 
   public renderCell = (index: number) => {
+    const { board } = this.state;
     return (
-      <div className="cell"></div>
+      <div className="cell" data-player={ board[index] } key={ index } onClick={ () => this.handleCellClick(index) }></div>
     )
   }
 
   public renderBoard = () => {
+    const { board } = this.state;
     return (
       <div className="board-container">
-        { this.state.board.map((value, key) => this.renderCell(key)) }
+        { board.map((value, key) => this.renderCell(key)) }
       </div>
     )
   }
